ab57b945760d0eeb88268baffa2b2e0f6ba040f7
harshnative <harshnative@gmail.com>
2022-02-16 17:51:34 +0530

    main.py for testing password manager
    Signed-off-by: harshnative <harshnative@gmail.com>

--- code_files/main.py
+++ code_files/main.py
@@ -1,16 +1,17 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from ui import m_passwordManager as test
+from ui.m_passwordManager import PasswordMainWidget as test
 from ui.packages.log_module.logger import Logger
 
 def handler(msg_type, msg_log_context, msg_string):
     pass
 
 QtCore.qInstallMessageHandler(handler)
-
+QtWidgets.QApplication.setStyle("fusion")
 app = QtWidgets.QApplication(sys.argv)
+
 Form = QtWidgets.QWidget()
-ui = test.mainScreenWidget(Logger("testLogs.log"))
+ui = test("./test.db" , Logger("testLogs.log"))
 ui.setupUi(Form)
 Form.show()
 rc = app.exec_()
