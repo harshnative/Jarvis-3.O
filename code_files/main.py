5ad0650e3d31274f7a5f202481c16ce5a32a08f3
harshnative <harshnative@gmail.com>
2022-02-18 16:34:45 +0530

    added main screen invoke from main.py
    Signed-off-by: harshnative <harshnative@gmail.com>

--- code_files/main.py
+++ code_files/main.py
@@ -1,28 +1,176 @@
+# essential modules
+import os
+import platform
+import time
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from ui.m_passwordManager import PasswordMainWidget as test
+from threading import Thread
+import multiprocessing
+import sys
+import pathlib
+import logging
+
+
+# declaring some global variables
+class GlobalData_main:
+
+    # current version of software
+    currentVersion = 0.1
+
+    # variables to determine the operating system of the user 
+    isOnWindows = False
+    isOnLinux = False
+
+    # variables to manage the loading animation 
+    runLoadingAnimation = True
+    loadingAnimationCount = 5
+    lAnimationObj = None
+
+
+
+    folderPathLinux = None
+    folderPathWindows = pathlib.Path("C:/programData/JarvisData").absolute()
+
+
+    loggerLevel = logging.INFO
+
+
+
+
+
+
+
+
+
+
+
+# Checking the users operating system and adding data to global class
+osUsing = platform.system()
+
+if(osUsing == "Linux"):
+    GlobalData_main.isOnLinux = True
+    
+    if(sys.argv[0].find(".py") != -1):
+        GlobalData_main.folderPathLinux = os.getcwd() + "/JarvisData"
+        GlobalData_main.loggerLevel = logging.DEBUG
+    else:
+        GlobalData_main.folderPathLinux = pathlib.Path("/opt/JarvisData").absolute()
+
+    # making the program data folder
+    try:
+        os.mkdir(GlobalData_main.folderPathLinux)
+    except FileExistsError:
+        pass
+    except PermissionError:
+        print(GlobalData_main.folderPathLinux)
+        print("\nPlease run jarvis using sudo")
+        sys.exit()
+
+
+elif(osUsing == "Windows"):
+    GlobalData_main.isOnWindows = True
+
+    # making the program data folder
+    try:
+        os.mkdir(GlobalData_main.folderPathWindows)
+    except FileExistsError:
+        pass
+
+else:
+    print("Jarvis currently does not support this operating system :(")
+    time.sleep(3)
+    sys.exit()
+
+del osUsing
+
+
+
+
+
+# loading animation thread
+class LoadingAnimation(Thread):
+
+    def run(self):
+
+        string = ""
+
+        # will run only till max 5 times or early if the global var runLoadingAnimation is made false
+        while(GlobalData_main.runLoadingAnimation and GlobalData_main.loadingAnimationCount):
+            string = string + "."
+            time.sleep(0.5)
+            print("\rloading , please wait " , string , end = "")
+            GlobalData_main.loadingAnimationCount -= 1
+
+        print()
+
+
+
+
 from ui.packages.log_module.logger import Logger
 
+
+# starting in the main to avoide awakeness by sub processes
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    if(GlobalData_main.loggerLevel != logging.DEBUG):
+        # loading animation thread started 
+        GlobalData_main.lAnimationObj = LoadingAnimation()
+        GlobalData_main.lAnimationObj.start()
+
+
+    if(GlobalData_main.isOnLinux):
+        loggerPath = pathlib.Path(GlobalData_main.folderPathLinux , "logs.log").absolute()
+    else:
+        loggerPath = pathlib.Path(GlobalData_main.folderPathWindows , "logs.log").absolute()
+
+    globalLogger = Logger(loggerPath , level=GlobalData_main.loggerLevel)
+
+
+
+
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui.m_mainScreen import MainScreenWidget as test
+
+
 def handler(msg_type, msg_log_context, msg_string):
-    pass
 
-QtCore.qInstallMessageHandler(handler)
-QtWidgets.QApplication.setStyle("fusion")
-app = QtWidgets.QApplication(sys.argv)
+    if(msg_string == "QWidget::paintEngine: Should no longer be called"):
+        pass
+    else:
+        globalLogger.loggerObj.warning(f"msg_type = {msg_type} , msg_log_context = {msg_log_context} , msg_string = {msg_string}")
+        globalLogger.print_log()
+        
 
-Form = QtWidgets.QWidget()
-ui = test("./test.db" , Logger("testLogs.log"))
-ui.setupUi(Form)
-Form.show()
-rc = app.exec_()
 
-print("DONE")
 
-del app
-del ui
-del Form
+if __name__ == "__main__":
+    QtCore.qInstallMessageHandler(handler)
+    QtWidgets.QApplication.setStyle("fusion")
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = test(globalLogger)
 
-sys.exit( rc )
+
+    if(GlobalData_main.loggerLevel != logging.DEBUG):
+        # closing the loading animation
+        GlobalData_main.runLoadingAnimation = False
+        GlobalData_main.lAnimationObj.join()
+
+
+    ui.setupUi(Form)
+    Form.show()
+    rc = app.exec_()
+
+    del app
+    del ui
+    del Form
+
+    sys.exit(rc)
+
+    
+
 
 
 
