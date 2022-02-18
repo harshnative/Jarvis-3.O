1a466cc08212f1cbd3ebe28ca087334bcc3e9d77
harshnative <harshnative@gmail.com>
2022-02-18 18:54:26 +0530

    shifted global data main from main.py to global data package
    Signed-off-by: harshnative <harshnative@gmail.com>

--- code_files/ui/packages/globalData/globalDataClasses.py
+++ code_files/ui/packages/globalData/globalDataClasses.py
@@ -1,3 +1,6 @@
+import pathlib
+import logging
+
 
 class GlobalDicts:
     pass
@@ -6,7 +9,36 @@ class GlobalDicts:
 class GlobalLists:
     modules_list = [
         "Password\nmanager" ,
+        "Settings" ,
     ]
+
+
+
+
+
+
+# declaring some global variables
+class GlobalData_main:
+
+    # current version of software
+    currentVersion = 0.1
+
+    # variables to determine the operating system of the user 
+    isOnWindows = False
+    isOnLinux = False
+
+    # variables to manage the loading animation 
+    runLoadingAnimation = True
+    loadingAnimationCount = 5
+    lAnimationObj = None
+
+
+
+    folderPathLinux = None
+    folderPathWindows = pathlib.Path("C:/programData/JarvisData").absolute()
+
+
+    loggerLevel = logging.INFO
 
 
 
